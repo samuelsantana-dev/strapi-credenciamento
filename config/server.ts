e2f2bc5b32bd9bd5f1bc01ea5716a699fec32ba2
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-05-06 14:18:33 -0300

    Chaves unicas

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,11 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: [
+      "oacmK17yjUKOOAXEhbdg2Q==",
+      "eeH/aLXPeH//luogMl3W+Q==",
+      "9fnAzfV2jbJWc/BPllckew==",
+      "PS6d84D2YbcztxQXJeKKhw=="
+    ],
   },
 });
